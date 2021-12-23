b87303f50f9fad30c617f60d4e173e5a1e29caf6
bonot9434 <bonot9434@gmail.com>
2021-12-23 21:57:01 +0900

    exam6

---
+++ exercise2/exam6.java
@@ -0,0 +1,16 @@
+class exam6 {
+  public static void main(String[] args) {
+    int x;
+
+    x = 7;
+    x = x * 3;
+    //x * = 3;
+
+    System.out.println("x="x);
+
+    x = x / 2;
+    //x / = 2;
+
+    System.out.println("x="x);
+  }
+}
