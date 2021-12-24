0a948c695b6799316a46741cc872783893fa22c6
bonot9434 <bonot9434@gmail.com>
2021-12-24 22:24:20 +0900

    exercise3追加

---
+++ exercise3/exam2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class exam2 {
+  public static void main( String[] args ) throws IOException{
+    BufferedReader br = new BufferedReader(
+                        new InputStreamReader( System.in ) );
+    int x = Integer.parseInt( br.readLine() );
+    System.out.println( x );
+  }
+}
