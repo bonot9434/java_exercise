7e1d6886557993b513fc5a01ea4d4a98b0e805f0
bonot9434 <bonot9434@gmail.com>
2021-12-25 15:11:34 +0900

    exercise4追加

---
+++ exercise4/exam3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class exam3 {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(
+                        new InputStreamReader( System.in ));
+
+    int x = Integer.parseInt( br.readLine() );
+    int y = Integer.parseInt( br.readLine() );
+
+    if (x>y)
+      System.out.println("xはyより大きい。");
+    else if (x<y)
+      System.out.println("xはyより小さい。");
+  }
+}
