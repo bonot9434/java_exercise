84ed677606ee1938a51bbfe8da08a706ab834a44
bonot9434 <bonot9434@gmail.com>
2021-12-26 23:03:40 +0900

    exercise5追加

---
+++ exercise5/exam2.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class exam2 {
+  public static void main(String[] args) throws IOException {
+    for (int i = 1; i <= 9; i++)
+      System.out.print((3*i)+ "");
+  }
+}
