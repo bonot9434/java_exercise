cfebdd24454c89f6863bdcec70610520b887644f
bonot9434 <bonot9434@gmail.com>
2021-12-28 14:11:10 +0900

    exercise5追加

---
+++ exercise5/exam13.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class exam13 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(
+                        new InputStreamReader(System.in));
+    int strike = 0;
+    int ball = 0;
+
+    while (strike < 3 && ball < 4) {
+      System.out.print("ストライク=1 or ボール=2 ?");
+
+      int judge = Integer.parseInt(br.readLine());
+
+      if (judge == 1)
+        strike++;
+      else if (judge == 2)
+        ball++;
+    }
+    System.out.println(ball + "ボール" + strike + "ストライク");
+  }
+}
